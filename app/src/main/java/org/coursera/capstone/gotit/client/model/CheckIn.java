7341e174fae744e1bd7a3d9f63bf0fddaa161ccb
Denis Trotsky <denveru@gmail.com>
2016-12-08 22:21:51 -0800

    Updated gradle. Removed unused imports.

--- app/src/main/java/org/coursera/capstone/gotit/client/model/CheckIn.java
+++ app/src/main/java/org/coursera/capstone/gotit/client/model/CheckIn.java
@@ -1,7 +1,5 @@
 package org.coursera.capstone.gotit.client.model;
 
-import org.apache.commons.lang3.text.StrBuilder;
-
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
